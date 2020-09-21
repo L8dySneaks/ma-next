a51b6295638747b439a175e59c2c6a5e8aba6a0a
Steph Symosko <steph.symosko@gmail.com>
2020-09-20 22:56:48 -0400

    tailwind css update to remove future breaking changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2b15c612362493fd6d5c";
+/******/ 	var hotCurrentHash = "d4b062e8184e6fc46b3d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
